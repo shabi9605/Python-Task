4c941c6721219a745ea9e2e3e39a947516428f60
unknown <shabeebshabi960580@gmail.com>
2023-05-22 17:25:58 +0530

    Created api for calculating value of in a quadratic equation in a sanic framework project

---
+++ sanic/calculate_x.py
@@ -0,0 +1,12 @@
+import math
+
+
+def calculate_x(a,b,c):
+    b_square = b**2
+    four_a_c = 4*a*c
+    if (b_square - four_a_c) < 0:
+        raise ValueError("Invalid Inputs")
+    square_root = math.sqrt(b_square - four_a_c)
+    x1 = (-b + square_root)/(2*a)
+    x2 = (-b - square_root)/(2*a)
+    return x1, x2
--- sanic/server.py
+++ sanic/server.py
@@ -1,5 +1,8 @@
 from sanic import Sanic
-from sanic.response import text
+from sanic.response import text,json
+from calculate_x import calculate_x
+from forms import QudraticModel
+from pydantic import ValidationError
 
 app = Sanic("MyHelloWorldApp")
 
@@ -10,8 +13,16 @@ async def hello_world(request):
 
 @app.post("/api/")
 async def quadraic_eqn(request):
-    print("request ===> ",request.json)
-    return text("Hello, world. shabeeb")
+    data = request.json
+    try:
+        data = QudraticModel(**data)
+    except ValidationError as e:
+        return json({"error":e.json()})
+    try:
+        x1,x2 = calculate_x(data.a,data.b,data.c)
+        return json({"x1":x1,"x2":x2})
+    except ValueError:
+        return json({"error":"Invalid Input"})
 
 
 
